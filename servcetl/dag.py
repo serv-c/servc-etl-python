f82a844d315b3449814506cf17b62cb486704c6c
Yusuf Ali <ali@yusuf.email>
2025-06-02 00:23:31 -0400

    fix(dag): add in other typings for components

--- servcetl/dag.py
+++ servcetl/dag.py
@@ -28,3 +28,41 @@ class Dag(BaseModel):
     steps: List[Step]
     notifiers: Notifier
     provisioner: List[ProvisionerComponent]
+
+
+class ProvisionerType(Enum):
+    DEPLOYMENT = "deployment"
+    CRON = "cron"
+
+
+class Resource(BaseModel):
+    cpu: str
+    memory: str
+
+
+class Resources(BaseModel):
+    requests: Resource
+    limits: Resource
+
+
+class EnvVariable(BaseModel):
+    name: str
+    value: str
+
+
+class Deployment(ProvisionerComponent):
+    image: str
+    resources: Resources
+    pullSecret: List[str]
+    env: List[EnvVariable]
+    queueName: str
+    maxReplicas: int
+
+
+class CronTrigger(ProvisionerComponent):
+    image: str
+    resources: Resources
+    pullSecret: List[str]
+    env: List[EnvVariable]
+    cron: str
+    cmd: List[str]
